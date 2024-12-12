78e7f3b1084665dc1e9f2bf2b03b3f94bfc864f7
RaimaAftab95 <raima.aftab2022@gmail.com>
2024-12-12 13:05:56 +0500

    Update AutoCounter.jsx
    to share theme changer context

--- react-hooks-fetch-example/src/components/AutoCounter.jsx
+++ react-hooks-fetch-example/src/components/AutoCounter.jsx
@@ -1,6 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext  } from "react";
+import { ThemeContext } from "../contexts/ThemeContext"; // Import the ThemeContext
 
 const AutoCounter = () => {
+
+  const { theme, toggleTheme } = useContext(ThemeContext); // Access theme from context
   const [count, setCount] = useState(0);
   const [isRunning, setIsRunning] = useState(true);
 
@@ -17,13 +20,19 @@ const AutoCounter = () => {
   const toggleCounter = () => setIsRunning(!isRunning);
 
   return (
-    <div className="p-4">
+    <div className={`p-4 ${theme === "light" ? "bg-white text-black" : "bg-gray-800 text-white"}`}>
       <h1 className="text-2xl">Auto Counter: {count}</h1>
       <button
         className="bg-blue-500 text-white px-4 py-2 rounded mt-2"
         onClick={toggleCounter}
       >
         {isRunning ? "Stop" : "Start"}
+      </button>
+       <button
+        className="ml-4 bg-green-500 text-white px-4 py-2 rounded mt-2"
+        onClick={toggleTheme}
+      >
+        Toggle Theme
       </button>
     </div>
   );
